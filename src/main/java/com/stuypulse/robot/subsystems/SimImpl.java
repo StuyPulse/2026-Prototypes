fabccdf0eb88a5b52c4701bc7d1fcb8b782beea2
Ryan Bergman <rbergman80@stuy.edu>
2026-02-07 01:00:27 -0500

    Progress made, alleged solution written down on paper. Will apply tomorrow morning or at the meeting. FEAT burgatrov

--- src/main/java/com/stuypulse/robot/subsystems/SimImpl.java
+++ src/main/java/com/stuypulse/robot/subsystems/SimImpl.java
@@ -31,7 +31,7 @@ public class SimImpl extends Sim{
         //doing Kraken because i remember the 0.001 value from its sim website
         //made random Gear ratio
         motor = new DCMotorSim(LinearSystemId.createDCMotorSystem(DCMotor.getKrakenX60(1), 0.001, 40), DCMotor.getKrakenX60(1));
-        pidController = new PIDController(0.8, 0, 0);
+        pidController = new PIDController(0.4, 0, 0);
         ffController = new ArmFeedforward(0, 0, 0, 0, 0.02);
 
         canvas = new Mechanism2d(4, 4);
@@ -39,6 +39,12 @@ public class SimImpl extends Sim{
         turret = rootVector.append(new MechanismLigament2d("actual turret representation", 2, 0));
 
         motor.setAngle(Math.toRadians(0));
+    }
+
+    public double getAbsoluteAngularPositionDeg() {
+        return Math.abs(Math.toDegrees(motor.getAngularPositionRad()) % 360); 
+        //TODO: fix Math.abs causing issue
+        //make it so that 0 wraps to 360 as well
     }
 
 
@@ -51,28 +57,41 @@ public class SimImpl extends Sim{
         useOther = false; //reset value each time
         //TODO: add the actual full turn counters, the negatives side counter or something, and just overall make it work.
         SmartDashboard.putData("turret visualizer", canvas);
-    
+        
+        double getAAPDeg = getAbsoluteAngularPositionDeg();
         //LOGIC GOES FROM HERE -> right now at find shortest path
-        if (getState().getAngle().get() >= Math.toDegrees(motor.getAngularPositionRad())) {
-            positiveAngle = getState().getAngle().get() - Math.toDegrees(motor.getAngularPositionRad()); //TODO: (IMPORTANT) MIGHT GO HORRIBLY WRONG AND IF SO, RESET STATICS AT START OF PERIODIC??
+        if (getState().getAngle().get() >= getAbsoluteAngularPositionDeg()) {
+            positiveAngle = getState().getAngle().get() - getAbsoluteAngularPositionDeg(); //TODO: (IMPORTANT) MIGHT GO HORRIBLY WRONG AND IF SO, RESET STATICS AT START OF PERIODIC??
             if (positiveAngle > 180 || (Math.toDegrees(motor.getAngularPositionRad()) + positiveAngle > 480) /*goes above threshold?*/) { //TODO: update with actual value
                 negativeAngle = -1 * (360 - positiveAngle);
                 useOther = true;
             }
 
-            pidOutput = pidController.calculate(Math.toDegrees(motor.getAngularPositionRad()), (useOther) ? Math.toDegrees(motor.getAngularPositionRad()) + negativeAngle :  Math.toDegrees(motor.getAngularPositionRad()) + positiveAngle);
+            if (useOther && (getAAPDeg + negativeAngle < 0)) {
+                //change getAAPDeg for PIDController -> apply to getAAPDeg the negative angle
+                getAAPDeg = -1000000; //placeholdere for commit just so i remember what i was doing
+            }
+            else {
+                getAAPDeg = getAbsoluteAngularPositionDeg() + negativeAngle;
+            }
+            //do the same here
+            //then input  
+
+            pidOutput = pidController.calculate(getAbsoluteAngularPositionDeg(), (useOther) ? getAAPDeg :  getAbsoluteAngularPositionDeg() + positiveAngle);
             //TODO: put pid stuff in here or make another boolean that shows which if we went through (target is greater or less than)
         }
 
-        else if (getState().getAngle().get() < Math.toDegrees(motor.getAngularPositionRad())) {
-            negativeAngle = getState().getAngle().get() - Math.toDegrees(motor.getAngularPositionRad());
+        else if (getState().getAngle().get() < getAbsoluteAngularPositionDeg()) {
+            negativeAngle = getState().getAngle().get() - getAbsoluteAngularPositionDeg();
             if (negativeAngle < -180 || (Math.toDegrees(motor.getAngularPositionRad()) + negativeAngle) < -480  /*goes above threshold?*/) {
                 positiveAngle = 360 + negativeAngle;
                 useOther = true;
-            } 
+            }
+            if (!useOther && true) {
+                //change getAAPDeg for PIDController
+            }
 
-            pidOutput = pidController.calculate(Math.toDegrees(motor.getAngularPositionRad()), (useOther) ? Math.toDegrees(motor.getAngularPositionRad()) + positiveAngle : Math.toDegrees(motor.getAngularPositionRad()) + negativeAngle); //TODO: (IMPORTANT) MIGHT GO HORRIBLY WRONG AND IF SO, RESET STATICS AT START OF PERIODIC??
-            //TODO: might have to negate voltage too?
+            pidOutput = pidController.calculate(getAbsoluteAngularPositionDeg(), (useOther) ? getAbsoluteAngularPositionDeg() + positiveAngle : getAAPDeg);
         }    
 
         double ffOutput = ffController.calculate(pidController.getSetpoint(), 1); //TODO: update the velocity value
@@ -87,7 +106,9 @@ public class SimImpl extends Sim{
 
 
         SmartDashboard.putNumber("Turret Simulation/ Sim Values/ Voltage", voltage);
-        SmartDashboard.putNumber("Turret Simulation/ Sim Values/ Motor Position", Math.toDegrees(motor.getAngularPositionRad()));
+        SmartDashboard.putNumber("Turret Simulation/ Sim Values/ ACTUAL Motor Position", Math.toDegrees(motor.getAngularPositionRad()));
+        SmartDashboard.putNumber("Turret Simulation/ Sim Values/ RELATIVE Motor Position", getAbsoluteAngularPositionDeg());
+
         SmartDashboard.putNumber("Turret Simulation/ Sim Values/ Random Angle", getState().getAngle().get());
         SmartDashboard.putNumber("Turret Simulation/ Sim Values/ PID output", pidOutput);
         SmartDashboard.putNumber("Turret Simulation/ Sim Values/ FF output", ffOutput);
@@ -97,7 +118,7 @@ public class SimImpl extends Sim{
         SmartDashboard.putNumber("Turret Simulation/ Sim Values/ Positive Angle", positiveAngle);
         SmartDashboard.putNumber("Turret Simulation/ Sim Values/ Negative Angle", negativeAngle);
 
-        SmartDashboard.putBoolean("Turret Simulation/ Sim Values/ Past limit or greater than 180?", positiveAngle > 180 || (Math.toDegrees(motor.getAngularPositionRad()) + positiveAngle) > 480);
-        SmartDashboard.putBoolean("Turret Simulation/ Sim Values/ Past limit or less than -180?", negativeAngle < -180 || (Math.toDegrees(motor.getAngularPositionRad()) + negativeAngle) < -480);
+        SmartDashboard.putBoolean("Turret Simulation/ Sim Values/ Past limit or greater than 180?", (Math.toDegrees(motor.getAngularPositionRad()) + positiveAngle) > 480);
+        SmartDashboard.putBoolean("Turret Simulation/ Sim Values/ Past limit or less than -180?", (Math.toDegrees(motor.getAngularPositionRad()) + negativeAngle) < -480);
     }
 }
